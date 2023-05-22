37a288166641e8805d87516cdd0a1fba82e87447
surya <suryaprakashnarayana@gmail.com>
2023-05-22 14:55:23 +0530

    Finalize

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -81,7 +81,7 @@ export const Deatils = () => {
                 </Grid>
 
                 <div className="tagLine">
-                    <p id="madeByMohit">Made with ❤️ by Mohit Sehrawat</p>
+                    <p id="madeByMohit">Made with ❤️ for PEC</p>
                 </div>
             </Box >
         </>
